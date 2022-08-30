dd73009fed05259d49ddc43466c6b107223ca401
Ramiro Nasello <rnasello@hotmail.com>
2022-08-29 21:07:14 -0300

    mi primer commit

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+
+
+const CartWidget = () => {
+  return (
+    
+  )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+
+
+const ItemListContainer = (prop) => {
+  return (
+    <h1>Tienda de F1</h1>
+  )
+}
+
+export default ItemListContainer
